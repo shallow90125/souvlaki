8945f9d7bfe1a792c8088e92a44d2ffc5d6c098f
shallow90125 <shallow90125@gmail.com>
2024-05-03 05:14:35 +0900

    add index page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,29 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Noto Sans JP']
+				sans: ['Noto Sans JP', ...defaultTheme.fontFamily.sans]
+			},
+			animation: {
+				gradient: 'gradient 60s linear infinite',
+				'main-fade': 'main-fade 2s'
+			},
+			keyframes: {
+				gradient: {
+					to: { 'background-position': '600% center' }
+				},
+				'main-fade': {
+					from: {
+						opacity: 0
+					},
+					to: {
+						opacity: 1
+					}
+				}
 			}
 		}
 	},
