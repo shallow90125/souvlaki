1a41469e4c0616f81f6cbe5cb14f39dbabb72b5b
shallow90125 <shallow90125@gmail.com>
2024-05-03 23:49:45 +0900

    add about and works

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const works = defineCollection({
 	type: 'content',
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional()
+		date: z.coerce.date(),
+		image: z.string()
 	})
 });
 
-export const collections = { blog };
+export const collections = { works };
